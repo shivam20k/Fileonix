c77e0c24a3b9cd535ab07da885666fa5f558a306
Your Name~Shivam <shivamkadam205@gmail.com>
2025-09-25 00:44:26 +0530

    yes

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,13 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 /// <reference path="./.next/types/routes.d.ts" />
-/// <reference types="next/navigation-types/compat/navigation" />
-
-// Allow CSS imports under strict noUncheckedSideEffectImports
-declare module '*.css' {
-  const content: { readonly [className: string]: string } | string;
-  export default content;
-}
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/pages/api-reference/config/typescript for more information.
