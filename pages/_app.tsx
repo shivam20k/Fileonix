4f1a50c809979eb3c5e56d8ef2090c60753bff02
Your Name~Shivam <shivamkadam205@gmail.com>
2025-09-30 23:18:26 +0530

    global change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app';
 import { useEffect } from 'react';
 import Layout from '@/components/Layout';
-import '@/styles/globals.css';
+import './globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
